100689737d6424689d3ae4a74c9f2a6f5004903d
unknown <stamatis.asterios@gmail.com>
2022-03-02 08:23:27 +0200

    BCrypt hashing encryption added

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -27,11 +28,6 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ['password'],
-});
-
 const User = new mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => {
@@ -47,17 +43,18 @@ app.get('/register', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password,
-  });
-
-  newUser.save((err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render('secrets');
-    }
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
+    newUser.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render('secrets');
+      }
+    });
   });
 });
 
@@ -70,9 +67,13 @@ app.post('/login', (req, res) => {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render('secrets');
-        }
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result === true) {
+            res.render('secrets');
+          } else {
+            res.send('<h1>Wrong Password !</h1');
+          }
+        });
       }
     }
   });
